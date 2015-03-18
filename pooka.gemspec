0f43c7859843516303899b83e3f850f98bb5a3c6
sugamasao <sugamasao@gmail.com>
2015-03-19 00:10:35 +0900

    add coverage

--- pooka.gemspec
+++ pooka.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'pry'
   spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'codeclimate-test-reporter'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'simplecov'
-SimpleCov.start do
-  add_filter '/vendor/'
-  add_filter '/spec/'
-end
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pooka'
